847a93c30c48d331c02f028e080a16d33f441b52
Mani <MP201937@reigate.ac.uk>
2020-11-26 19:39:35 +0000

    Rock Paper Scissors thats it

---
+++ src/rockpaperscissors_game/RockPaperScissors_game.java
@@ -0,0 +1,28 @@
+package rockpaperscissors_game;
+
+import java.awt.Toolkit;
+
+public class RockPaperScissors_game {
+
+    public static void main(String[] args) {
+        IntroScreen I = new IntroScreen();
+        
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int xSize = (int)tk.getScreenSize().getWidth();
+        int ySize = (int)tk.getScreenSize().getHeight();
+        
+        I.setSize(1295,780);
+        I.setVisible(true);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
